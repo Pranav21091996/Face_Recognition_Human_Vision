08ace69ced02943482f6b66fad8b5e85e072bc7d
Pranav Agarwal <pranav2109@hotmail.com>
2018-10-27 14:52:09 +0530

    Update UpdatedEccentricityCNN.py

--- UpdatedEccentricityCNN.py
+++ UpdatedEccentricityCNN.py
@@ -66,6 +66,7 @@ test_label5 = [Dataset5_Label[i] for i in perm[int(0.8*len(Dataset5_Input)):]]
 num_crops = 4
 img_size = 64
 i=0
+num_classes = max(Dataset1_Label)+1
 
 TRAINING_EPOCHS = 10000
 BATCH_SIZE = 98
@@ -76,12 +77,12 @@ index_in_epoch = 0
 Data = InputImage
 Labels = Labels
 
-one_hot_labels = one_hot_encode(Labels,max(Dataset1_Label)+1)
-one_hot_labels_test1 = one_hot_encode(test_label1,max(Dataset1_Label)+1)
-one_hot_labels_test2 = one_hot_encode(test_label2,max(Dataset1_Label)+1)
-one_hot_labels_test3 = one_hot_encode(test_label3,max(Dataset1_Label)+1)
-one_hot_labels_test4 = one_hot_encode(test_label4,max(Dataset1_Label)+1)
-one_hot_labels_test5 = one_hot_encode(test_label5,max(Dataset1_Label)+1)
+one_hot_labels = one_hot_encode(Labels,num_classes)
+one_hot_labels_test1 = one_hot_encode(test_label1,num_classes)
+one_hot_labels_test2 = one_hot_encode(test_label2,num_classes)
+one_hot_labels_test3 = one_hot_encode(test_label3,num_classes)
+one_hot_labels_test4 = one_hot_encode(test_label4,num_classes)
+one_hot_labels_test5 = one_hot_encode(test_label5,num_classes)
 
 train_Data,train_label = shuffle(Data,one_hot_labels,random_state=20)
 test_Data1,test_label1 = shuffle(test_Data1,one_hot_labels_test1,random_state=20)
@@ -101,7 +102,7 @@ print("size of the test data5", len(test_Data5))
 VALIDATION_SIZE = len(test_Data1)
 
 x = neural_net_image_input()
-y = neural_net_label_input(100)    
+y = neural_net_label_input(num_classes)    
 keep_prob = neural_net_keep_prob_input()
 
 logits = conv_net(x, keep_prob)
